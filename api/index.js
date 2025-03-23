b639421b434264c7159d9878c01f58abec5635b2
Jai <rajput.jai110@gmail.com>
2025-03-23 22:16:56 +0530

    index change

--- api/index.js
+++ api/index.js
@@ -1,12 +1,10 @@
 const express = require("express");
-// const route = require("./routes/route"); // Ensure this path is correct
-// const { connectDB } = require("./config/db");
+const route = require("./routes/route"); // Ensure this path is correct
+const { connectDB } = require("./config/db");
 
 const app = express();
-// connectDB();
-app.get("/", (req, res) => {
-    res.send("hello node");
-  });
-app.listen(3000, () => console.log("Server ready on port 3000."));
+connectDB();
+app.use("/", route)
+app.listen(3001, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
