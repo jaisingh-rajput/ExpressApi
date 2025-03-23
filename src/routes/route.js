401759f43f778e75290c7623d847ca89ca9cf259
Jai <rajput.jai110@gmail.com>
2025-03-23 20:51:02 +0530

    route fix

--- src/routes/route.js
+++ src/routes/route.js
@@ -5,9 +5,9 @@ const user_login = require("../services/user_login")
 const update_user = require("../services/update_user")
 const get_user = require("../services/get_user")
 
-router.post("/register_user", register_user)
-router.post("/user_login", user_login)
-router.put("/update_user", update_user)
-router.get("/get_user", get_user)
+router.post("register_user", register_user)
+router.post("user_login", user_login)
+router.put("update_user", update_user)
+router.get("get_user", get_user)
 
 module.exports = router;
