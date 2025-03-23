7276433b5c698d162923e35ebb3007a9071e485b
Jai <rajput.jai110@gmail.com>
2025-03-23 22:18:23 +0530

    add all routes

--- api/routes/route.js
+++ api/routes/route.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const router = express.Router()
-// const register_user = require("../services/register_user")
-// const user_login = require("../services/user_login")
-// const update_user = require("../services/update_user")
-// const get_user = require("../services/get_user")
+const register_user = require("../services/register_user")
+const user_login = require("../services/user_login")
+const update_user = require("../services/update_user")
+const get_user = require("../services/get_user")
 
-// router.post("register_user", register_user)
-// router.post("user_login", user_login)
-// router.put("update_user", update_user)
-// router.get("get_user", get_user)
+router.post("register_user", register_user)
+router.post("user_login", user_login)
+router.put("update_user", update_user)
+router.get("get_user", get_user)
 
 router.get("", (req, res) => {
     res.send("hello node");
