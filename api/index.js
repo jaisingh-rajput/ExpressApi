4622c227d10f29a6e48e73ba092c07fa00dfdb75
Jai <rajput.jai110@gmail.com>
2025-03-23 21:47:35 +0530

    connectDb

---
+++ api/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const route = require("./routes/route"); // Ensure this path is correct
+const { connectDB } = require("./config/db");
+
+const app = express();
+connectDB();
+app.get("/", route);
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
