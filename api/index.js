a7ca057841a605a39104815af39032fb7fa0ac86
Jai <rajput.jai110@gmail.com>
2025-03-23 21:58:54 +0530

    index

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const serverless = require("serverless-http");
 const route = require("./routes/route"); // Ensure this path is correct
 const { connectDB } = require("./config/db");
 
@@ -7,4 +8,4 @@ connectDB();
 app.use("/", route)
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
-module.exports = app;
+module.exports = serverless(app); 
